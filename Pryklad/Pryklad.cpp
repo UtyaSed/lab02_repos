0f53e8e8eb3904c39aa83bf31aa1b80c15f774c7
Vikor <vbonchykv@gmail.com>
2020-10-26 18:17:11 +0200

    first branch_02 commit

--- Pryklad/Pryklad.cpp
+++ Pryklad/Pryklad.cpp
@@ -10,17 +10,17 @@ int main()
 {
 	double Pi = 4 * atan(1.); // число пі
 	double a; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	// double z2; // результат обчислення 2-го виразу
+	// double z1; // результат обчислення 1-го виразу
+	 double z2; // результат обчислення 2-го виразу
 
 	cout << "a = "; cin >> a;
 
-	z1 = (sin(Pi / 2 + 3 * a)) / (1 - sin(3 * a - Pi));
-	// z2 = 1 / tan(5 / 4 * Pi + 3 / 2 * a);
+	// z1 = (sin(Pi / 2 + 3 * a)) / (1 - sin(3 * a - Pi));
+	 z2 = 1 / tan(5 / 4 * Pi + 3 / 2 * a);
 
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	// cout << "z1 = " << z1 << endl;
+	 cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
